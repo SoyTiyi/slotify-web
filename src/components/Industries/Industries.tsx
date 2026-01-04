cfd5e896a74e96482538b02bfeb4259c333ce823
SoyTiyi <santiago.marmartinez@gmail.com>
2026-01-04 16:42:22 -0500

    add Industries

---
+++ src/components/Industries/Industries.tsx
@@ -0,0 +1,27 @@
+const Industries = () => {
+  return (
+    <div>
+      <div>
+        
+      </div>
+
+      <div className="flex flex-col text-white">
+        <h3>Adaptado a tu industria</h3>
+
+        <p>
+          No importa si cortas cabello, realizas camisas o atiendes pacientes.
+          Slotify se configura segun las necesidades de tu rubro para que no
+          tengas que cambiar tu forma de trabajar.
+        </p>
+
+        <div>
+            <li>Configuracion de horarios flexibles</li>
+            <li>Gestion de multiples sucursales</li>
+            <li>Perfiles de Staff con perfiles personalizados</li>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Industries;

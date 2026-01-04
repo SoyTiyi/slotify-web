eb60578583501cc9e49ef41d7e186fa235b9358b
SoyTiyi <santiago.marmartinez@gmail.com>
2026-01-04 14:44:10 -0500

    chage color on mobile dropdown menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -38,17 +38,17 @@ const Navbar = () => {
 
             <div className="hidden md:flex space-x-2">
                 <button className="bg-secondary font-semibold text-muted px-4 py-2 rounded ml-2 hover:text-primary cursor-pointer">Ingresar</button>
-                <button className="bg-primary font-bold text-white px-6 py-2 rounded-xl cursor-pointer">Prueba gratis</button>
+                <button className="bg-primary font-bold text-white px-6 py-2 rounded-xl cursor-pointer hover:bg-primary-hover">Prueba gratis</button>
             </div>
 
             {isOpen && (
-                <div className="absolute top-full left-0 w-full bg-base flex flex-col items-center py-4 space-y-4 md:hidden shadow-lg z-50 border-t border-gray-700">
+                <div className="absolute top-full left-0 w-full bg-surface flex flex-col items-center py-4 space-y-4 md:hidden shadow-lg z-50 border-t border-gray-700">
                     {links.map((link) => (
                         <NavbarLink key={link.id} href={link.href} label={link.label} />
                     ))}
                     <div className="flex flex-col space-y-2 w-full px-8">
                         <button className="bg-secondary font-semibold text-muted px-4 py-2 rounded hover:text-primary cursor-pointer w-full">Ingresar</button>
-                        <button className="bg-primary font-bold text-white px-6 py-2 rounded-xl cursor-pointer w-full">Prueba gratis</button>
+                        <button className="bg-primary font-bold text-white px-6 py-2 rounded-xl cursor-pointer w-full hover:bg-primary-hover">Prueba gratis</button>
                     </div>
                 </div>
             )}

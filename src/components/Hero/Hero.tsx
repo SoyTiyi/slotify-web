c81379b8c1685ca4192284cc313dfc78a5c66a66
Santiago Martínez Martínez <46984191+SoyTiyi@users.noreply.github.com>
2026-01-04 11:56:38 -0500

    fix botton responsive desing on small screnns

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center text-center text-white gap-8 py-18">
+    <div className="flex flex-col items-center text-center text-white gap-8 py-24">
       <div className="flex text-primary bg-primary/10 rounded-full px-4 py-2 items-center border-primary border">
         <div className="w-2 h-2 bg-primary rounded-full mr-2" />
         <p className="font-medium">Nuevo: Integracion con Whatsapp Business</p>
@@ -20,7 +20,7 @@ const Hero = () => {
         necesitas para tu negocio en una sola plataforma
       </p>
 
-      <div className="flex gap-4 mt-4 items-center">
+      <div className="flex flex-col gap-4 mt-4 items-center md:flex-row sm:flex-col sm:gap-3">
         <button className="flex text-text-primary bg-primary px-10 py-3 rounded-xl font-semibold hover:bg-primary-hover transition cursor-pointer">
           Prueba gratis
           <svg
